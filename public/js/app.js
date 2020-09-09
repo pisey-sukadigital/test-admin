4e6764eca8a65c6e01735a2787fca0e6f904d83b
oupiseyit <oupisey.it@gmail.com>
2020-09-09 21:10:28 +0700

    - fix style scss

--- public/js/app.js
+++ public/js/app.js
@@ -49897,8 +49897,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/user/Documents/Pisey/Code/Laravel/laravel-adminlte-vue/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/user/Documents/Pisey/Code/Laravel/laravel-adminlte-vue/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/Laravel-vue/laravel-adminlte-vue/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/Laravel-vue/laravel-adminlte-vue/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
