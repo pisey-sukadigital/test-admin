7247f78c3c07d395efdf68c4bd2a7a69b4c6dcb9
pisey <pisey.sukadigital@gmail.com>
2020-09-21 10:01:23 +0700

    - update api

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -17,44 +17,31 @@
 <body class="hold-transition sidebar-mini">
     <div id="app" class="wrapper">
         <!-- Content Header (Page header) -->
-            {{--  <navbar></navbar>  --}}
         @include('layouts.includes.navbar')
         <!-- /.content-header -->
 
         <!-- Main Sidebar Container -->
-            {{--  <main-sidebar-container></main-sidebar-container>  --}}
         @include('layouts.includes.main-sidebar-container')
         <!-- /.Main Sidebar Container -->
     
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper pt-2">
             <!-- Content Header (Page header) -->
-            {{--  <header-page></header-page>  --}}
             @include('layouts.includes.header')
             <!-- /.content-header -->
-
-            {{--  <div id="app">  --}}
-                <app></app>
-            {{--  </div>  --}}
-            
+              
             <!-- Main content -->
-                {{--  <router-view></router-view>  --}}
+            <app></app>
             <!-- /.content -->
-
-            {{--  <app></app>  --}}
-
         </div>
         <!-- /.content-wrapper -->
 
         <!-- Control Sidebar -->
-        {{--  <control-sidebar></control-sidebar>  --}}
         @include('layouts.includes.control-sidebar')
         <!-- /.control-sidebar -->
 
         <!-- Main Footer -->
-        {{--  <main-footer></main-footer>  --}}
         @include('layouts.includes.main-footer')
-
         <vue-progress-bar></vue-progress-bar>
     
     </div>
