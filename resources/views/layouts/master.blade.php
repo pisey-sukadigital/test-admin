c239dc74d6f0bcaad2212c674e8842403b2d453c
pisey <pisey.sukadigital@gmail.com>
2020-09-23 15:14:09 +0700

    - add debugbar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-    <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}">
+    <link rel="shortcut icon" href="{{ asset('img/icon/favicon.ico') }}">
 
     <title>Laravel adminlte vue</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
